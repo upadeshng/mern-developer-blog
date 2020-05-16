3ac11980d8c035634130b69edfbd448b084d5167
Upadesh Bhatta <upadesh.ng@gmail.com>
2020-05-16 14:29:50 +0545

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,7 +10,7 @@ connectDB();
 // Init Middleware, to allow to get data in req.body in api request
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
+// app.get('/', (req, res) => res.send('API running'));
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -17,6 +18,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // port
 const PORT = process.env.PORT || 5000;
 
